62a3771b171af0cbff297acf9441d7d86457444b
Paul Ganea <pegleg2060@gmail.com>
2016-10-09 23:14:41 -0400

    Added build script to get dependencies

---
+++ build.sh
@@ -0,0 +1,15 @@
+CURDIR=${pwd}
+git submodule update --init --recursive
+mkdir $CURDIR/deps/libgit2/build
+cd $CURDIR/deps/libgit2/build
+cmake -DTHREADSAFE=ON -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS=-fPIC -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DCMAKE_INSTALL_PREFIX=../install -DCURL=OFF .. && cmake --build .
+mkdir -p $CURDIR/deps/re2/build
+cd $CURDIR/deps/re2/build
+make test
+make install
+make testinstall
+cd $CURDIR
+mkdir build
+cd build
+cmake ..
+make
