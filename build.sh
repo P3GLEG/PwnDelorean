50a04746b0ffc4a1870f60bed420437f7a402b0f
Paul Ganea <pegleg2060@gmail.com>
2016-10-10 03:07:59 -0400

    fixed bug in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-CURDIR=${pwd}
+CURDIR=`pwd`
 git submodule update --init --recursive
-mkdir $CURDIR/deps/libgit2/build
+mkdir -p $CURDIR/deps/libgit2/build
 cd $CURDIR/deps/libgit2/build
 cmake -DTHREADSAFE=ON -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS=-fPIC -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DCMAKE_INSTALL_PREFIX=../install -DCURL=OFF .. && cmake --build .
 mkdir -p $CURDIR/deps/re2/build
